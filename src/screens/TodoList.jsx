174b3342372f73aebc4be8949702f155c1ae427f
Théo Pantecouteau <theo.pantecouteau@icloud.com>
2022-12-04 22:15:53 +0100

    Navigation

---
+++ src/screens/TodoList.jsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+
+const TodoList = ({navigation}) => {
+    return(
+        <View>
+            <Text>
+                Voici la Todo List
+            </Text>
+        </View>
+    )
+}
+
+export default TodoList;
