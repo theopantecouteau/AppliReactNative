fdfeba180e5b9cfbc28bba6671f6b5807d7b5d61
Hugo SEQUIER <hugoz@L450G8002.square-it.grp>
2022-12-07 18:10:09 +0100

    Ajout de ce que j'ai fais sur la TodoList

--- src/screens/TodoList.jsx
+++ src/screens/TodoList.jsx
@@ -1,14 +1,88 @@
-import * as React from 'react';
-import { View, Text } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { Button,StyleSheet, Text, View } from 'react-native';
+import React, { Component, useState, useEffect } from "react";
 
-const TodoList = ({navigation}) => {
-    return(
-        <View>
+const TodoList = ({navigation})  => {
+
+    const [isCreate, setIsCreate] = useState(false);
+    const [listToDo, setListToDo] = useState([]);
+    
+    const stateTache = [
+        "ToDo",
+        "Pending",
+        "Done"
+    ];
+
+    class Tache extends React.Component {
+        state = { 
+            currentState : 0,
+            nom : this.props.nom,
+            membres : this.props.membres,
+            desc : this.props.desc,
+            date : this.props.date,
+            checklist : [],
+            piecejointe : this.props.piecejointe,
+            url : this.props.url,
+        };
+
+        render() {
+            return (
+                <Text>{this.state.nom}</Text>
+            );
+        }
+    }
+
+    function createTache(){
+        setIsCreate(true);
+        return (
+            <Text></Text>
+        );
+    }
+
+    return (
+      <View style={styles.container}>
+        {!isCreate && (<Button 
+            onPress={createTache}
+            title="Créer une nouvelle Tâche"
+             color="#841584"
+        />)}
+        {!isCreate && listToDo.length > 0 && (
             <Text>
-                Voici la Todo List
+                {listToDo.map(value => {
+                    return <Text>{value}</Text>
+                })}
             </Text>
-        </View>
-    )
+        )}
+        {!isCreate && listToDo.length == 0 && (
+            <Text>
+                Aucune Tâche de créer.
+            </Text>
+        )}
+        {isCreate && (
+            <Button
+                onPress={() => {
+                    listToDo.push(<Tache nom="Hugo"/>)
+                    setIsCreate(false);
+                }}
+                title="Ajouter la tache"
+            />
+        )}
+      </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop : 50,
+  },
+  test: {
+      color: 'red',
+      fontWeight: 'bold',
+      fontSize: 30,
+  },
+  header : {
+    backgroundColor : 'red'
+  }
+});
 
 export default TodoList;
